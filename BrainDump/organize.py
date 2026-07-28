c44948af864b1f912afd7ae5f3e2f821165d1cb5
MiniJohan <163442688+MiniJohan@users.noreply.github.com>
2026-07-28 23:43:01 +0200

    Delete BrainDump directory

--- BrainDump/organize.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def organize(raw_text):
-    items = re.split(r'[,;\n]+', raw_text)
-    items = [item.strip() for item in items]
-    items = [re.sub(r'^[-•*–]\s*', '', item) for item in items]
-    items = [item for item in items if item]
-    return items
